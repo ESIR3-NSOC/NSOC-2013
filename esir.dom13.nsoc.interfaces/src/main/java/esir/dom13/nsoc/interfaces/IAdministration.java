f51873f24df588e2f8e6e011b011dd2e40b536d9
Clem35 <briand.clement@orange.fr>
2013-11-12 14:51:44 +0100

    Initial Commit

---
+++ esir.dom13.nsoc.interfaces/src/main/java/esir/dom13/nsoc/interfaces/IAdministration.java
@@ -0,0 +1,11 @@
+package esir.dom13.nsoc.interfaces;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Clement
+ * Date: 12/11/13
+ * Time: 14:51
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IAdministration {
+}
