431c83d88579a89163d552df30e0ddd2302eac3c
Renaud C <renaud_ren@hotmail.fr>
2013-11-12 15:08:41 +0100

    création interface

---
+++ esir.dom13.nsoc.interfaces/src/main/java/esir/dom13/nsoc/interfaces/IScheduleDoor.java
@@ -0,0 +1,11 @@
+package esir.dom13.nsoc.interfaces;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Renaud
+ * Date: 12/11/13
+ * Time: 14:59
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IScheduleDoor {
+}
