0850c9dd6b68cc041bada01224235dc068a67815
ChappouxR <chappoux.renaud@gmail.com>
2013-11-12 15:24:39 +0100

    creation interface 2

--- esir.dom13.nsoc.interfaces/src/main/java/esir/dom13/nsoc/interfaces/IManagementBooking.java
+++ esir.dom13.nsoc.interfaces/src/main/java/esir/dom13/nsoc/interfaces/IManagementBooking.java
@@ -4,9 +4,8 @@ package esir.dom13.nsoc.interfaces;
  * Created with IntelliJ IDEA.
  * User: Renaud
  * Date: 12/11/13
- * Time: 15:00
+ * Time: 15:17
  * To change this template use File | Settings | File Templates.
  */
-public interface ISupervisor {
-
+public interface IManagementBooking {
 }
