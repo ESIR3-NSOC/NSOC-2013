81a95a4f6f3cad5c935799b0d3a0e1db7bc32474
ChappouxR <chappoux.renaud@gmail.com>
2013-11-12 15:33:49 +0100

    creation interface 3

---
+++ esir.dom13.nsoc.interfaces/src/main/java/esir/dom13/nsoc/interfaces/IManagementSensor.java
@@ -0,0 +1,11 @@
+package esir.dom13.nsoc.interfaces;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Renaud
+ * Date: 12/11/13
+ * Time: 15:29
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IManagementSensor {
+}
