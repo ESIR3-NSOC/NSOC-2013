b7ae4960891080df3098ec6799225f0e181a9acc
ChappouxR <chappoux.renaud@gmail.com>
2014-02-11 16:35:00 +0100

    changer appel bdd

--- esir.dom13.nsoc.adminDatabaseBuilding/src/main/java/esir/dom13/nsoc/adminDatabaseBuilding/adminDatabaseBuilding.java
+++ esir.dom13.nsoc.adminDatabaseBuilding/src/main/java/esir/dom13/nsoc/adminDatabaseBuilding/adminDatabaseBuilding.java
@@ -5,8 +5,9 @@ import esir.dom13.nsoc.database.IDatabaseConnection;
 import org.json.JSONArray;
 import org.kevoree.annotation.*;
 import org.kevoree.framework.AbstractComponentType;
+import org.kevoree.log.Log;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,26 +26,74 @@ import java.sql.SQLException;
         @RequiredPort(name = "connectDatabase", type = PortType.SERVICE, className = IDatabaseConnection.class)
 })
 
+@DictionaryType({
+        @DictionaryAttribute(name = "JDBC_DRIVER", defaultValue = "com.mysql.jdbc.Driver", optional = false),
+        @DictionaryAttribute(name = "DB_URL", defaultValue = "jdbc:mysql://148.60.11.209/projetnsoc", optional = false),
+        @DictionaryAttribute(name = "USER", defaultValue = "user", optional = false),
+        @DictionaryAttribute(name = "PASS", defaultValue = "ydr2013.", optional = false)
+})
+
 @ComponentType
 public class adminDatabaseBuilding extends AbstractComponentType implements IadminDatabaseBuilding {
 
 
+    // JDBC driver name and database URL
+    private String JDBC_DRIVER, DB_URL;
+
+    //  Database credentials
+    private String USER, PASS;
+    private Connection connection;
+
+
     @Start
     public void start() {
-
+        JDBC_DRIVER = getDictionary().get("JDBC_DRIVER").toString();
+        DB_URL = getDictionary().get("DB_URL").toString();
+        USER = getDictionary().get("USER").toString();
+        PASS = getDictionary().get("PASS").toString();
+        //STEP 2: Register JDBC driver
+        try {
+            Class.forName(JDBC_DRIVER);
+            //STEP 3: Open a connection
+            Log.debug("Connecting to database...");
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            Log.debug("Connecting to database...  OK");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Stop
     public void stop() {
-
-
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @Update
     public void update() {
+        JDBC_DRIVER = getDictionary().get("JDBC_DRIVER").toString();
+        DB_URL = getDictionary().get("DB_URL").toString();
+        USER = getDictionary().get("USER").toString();
+        PASS = getDictionary().get("PASS").toString();
 
-
+        try {
+            connection.close();
+            Class.forName(JDBC_DRIVER);
+            //STEP 3: Open a connection
+            Log.debug("Connecting to database ::: IDatabasePeople");
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Port(name = "setDatabaseBuilding", method = "addBuilding")
@@ -92,15 +141,24 @@ public class adminDatabaseBuilding extends AbstractComponentType implements Iadm
 
         String value = null;
         JSONArray tableau = new JSONArray();
-        String sql = "SELECT `nameBuilding` FROM `idatabasebuilding`";
-        CachedRowSetImpl rs = null;
-        rs = getPortByName("connectDatabase", IDatabaseConnection.class).sendRequestToDatabase(sql);
+        String request = "SELECT `nameBuilding` FROM `idatabasebuilding`";
+        Log.debug(request);
+        Statement stmt = null;
 
         try {
+            //STEP 4: Execute a query
+            Log.debug("Creating statement...");
+            stmt = connection.createStatement();
+            Log.debug("Execution of :: "+request);
+
+            ResultSet rs = stmt.executeQuery(request);
+
+
             while (rs.next()){
                 value = rs.getString("nameBuilding");   //voir si cela fonctionne et qu'on récupère valeur Bdd
                 tableau.put(value);
             }
+            stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
@@ -114,17 +172,25 @@ public class adminDatabaseBuilding extends AbstractComponentType implements Iadm
 
         String value = null;
         JSONArray tableau = new JSONArray();
-        String sql = "SELECT * FROM `idatabasebuilding` WHERE nameBuilding = '" + nameBuilding + "'";
-        CachedRowSetImpl rs = null;
-        rs = getPortByName("connectDatabase", IDatabaseConnection.class).sendRequestToDatabase(sql);
+        String request = "SELECT * FROM `idatabasebuilding` WHERE nameBuilding = '" + nameBuilding + "'";
+        Log.debug(request);
+        Statement stmt = null;
 
         try {
+            //STEP 4: Execute a query
+            Log.debug("Creating statement...");
+            stmt = connection.createStatement();
+            Log.debug("Execution of :: "+request);
+
+            ResultSet rs = stmt.executeQuery(request);
+
             while(rs.next()){
             value = rs.getString("id_building");   //voir si cela fonctionne et qu'on récupère valeur Bdd
                 tableau.put(value);
                 value = rs.getString("numberOfRoom");   //voir si cela fonctionne et qu'on récupère valeur Bdd
                 tableau.put(value);
             }
+            stmt.close();
             }
          catch (SQLException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
